b54ce9833e81a995c72fdb414e17bc8a9d88ce93
Matt Murch <mattmurch@gmail.com>
2017-06-21 13:56:12 -0400

    Add LICENSE.md and setup.py files.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = '0.10'
+
+setup(name='GuessThatSong',
+      version=__version__,
+      install_requires=['Flask==0.12.1',
+					'SQLAlchemy>=1.1.9'],
+      description='Song Trivia Game Web Application',
+      long_description=open('README.md').read(),
+      author='Matt Murch',
+      author_email='mattmurch@gmail.com',
+      url='https://github.com/mattmurch/GuessThatSong',
+      download_url='https://github.com/mattmurch/GuessThatSong/tarball/' + __version__,
+      scripts=['GuessThatSong/run.py'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Framework :: Flask',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Topic :: Games/Entertainment',
+      ],
+      keywords=('music trivia guessing game'),
+      packages=['app',
+            'db_repository',
+      ],
+      )
