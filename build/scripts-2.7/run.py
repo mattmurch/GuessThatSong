6a8852a1b114576e8169a223559e4b539644b161
Matt Murch <mattmurch@gmail.com>
2017-07-08 15:11:20 -0400

    Remove excess files and cleaned static directory

--- build/scripts-2.7/run.py
+++
@@ -1,6 +0,0 @@
-#!/home/matt/Work/GuessThatSong/venv/bin/python
-
-from app import app, db
-
-
-app.run(debug=True)
--- venv/bin/run.py
+++
@@ -1,5 +0,0 @@
-#!/home/matt/Work/GuessThatSong/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'GuessThatSong==0.1.0','run.py'
-__requires__ = 'GuessThatSong==0.1.0'
-import pkg_resources
-pkg_resources.run_script('GuessThatSong==0.1.0', 'run.py')
